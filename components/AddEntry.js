d49bd81d49e60e3357a2dd27aa4f533f86e11db0
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-12-28 16:21:54 +0530

    Show all metric icons

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -43,9 +43,20 @@ export default class AddEntry extends React.Component {
     }
 
     render() {
+        const metaInfo = getMetricMetaInfo()
+
         return (
             <View>
-                {getMetricMetaInfo('sleep').getIcon()}
+                {Object.keys(metaInfo).map((key) => {
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = this.state[key]
+
+                    return (
+                        <View key={key}>
+                            {getIcon()}
+                        </View>
+                    )
+                })}
             </View>
         )
     }
